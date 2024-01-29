ddea9c532339aaebc1b2708c8fdd4793c51d5c2c
Ohana Luz <123988706+ohanaluz@users.noreply.github.com>
2024-01-29 01:52:36 -0300

    Add files via upload
    Erro no botão "Update" corrigido. Anteriormente o update não era feito e a tela não progredia. Agora está seguindo corretamente.

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,33 +1,33 @@
- document.addEventListener('DOMContentLoaded', function () {
-            showData();
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    showData();
+});
 
-function validateForm (){
+function validateForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var age = document.getElementById("age").value;
     var adress = document.getElementById("adress").value;
 
-    if (name ==""){
+    if (name === "") {
         alert('Por favor, informe o nome');
         return false;
     }
 
-    if (email ==""){
+    if (email === "") {
         alert('Por favor, informe o email');
         return false;
     } else if (!email.includes('@')) {
-        alert ('Por favor, insira um email válido');
+        alert('Por favor, insira um email válido');
         return false;
     }
-    if (age ==""){
+    if (age === "") {
         alert('Por favor, informe a idade');
         return false;
-    } else if (age <=0) {
-        alert ("Por favor, insira uma idade válida");
+    } else if (age <= 0) {
+        alert("Por favor, insira uma idade válida");
         return false;
     }
-    if (adress ==""){
+    if (adress === "") {
         alert('Por favor, informe o endereço');
         return false;
     }
@@ -35,104 +35,72 @@ function validateForm (){
     return true;
 }
 
-function showData(){
-    var peopleList;
-    if (localStorage.getItem('peopleList')==null){
-        peopleList = [];
-    } else {
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
-    }
+function showData() {
+    var peopleList = JSON.parse(localStorage.getItem('peopleList')) || [];
+    var html = "";
 
-    var html ="";
-
-    peopleList.forEach(function (element, index){
+    peopleList.forEach(function (element, index) {
         html += "<tr>";
         html += "<td>" + element.name + "</td>";
         html += "<td>" + element.email + "</td>";
         html += "<td>" + element.age + "</td>";
         html += "<td>" + element.adress + "</td>";
-        html += '<td><button onclick="deleteData('+index+')" class="btn btn-danger">Apagar dados</button> <button onclick="updateData('+index+')"    class="btn btn-warning m-2">Atualizar dados</button> </td>';
-        html +="</tr>"
-        
+        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Apagar dados</button> <button onclick="updateData(' + index + ')" class="btn btn-warning m-2">Atualizar dados</button> </td>';
+        html += "</tr>";
     });
 
     document.querySelector('#crudTable tbody').innerHTML = html;
 }
 
-document.onload = showData();
-
-
-function AddData (){
-    if (validateForm() == true){
+function AddData() {
+    if (validateForm()) {
         var name = document.getElementById('name').value;
         var email = document.getElementById('email').value;
         var age = document.getElementById('age').value;
         var adress = document.getElementById('adress').value;
 
-        var peopleList;
-    if (localStorage.getItem('peopleList')==null){
-        peopleList = [];
-    } else {
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
+        var peopleList = JSON.parse(localStorage.getItem('peopleList')) || [];
+        peopleList.push({
+            name: name,
+            email: email,
+            age: age,
+            adress: adress
+        });
+
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("adress").value = "";
     }
-
-    peopleList.push ({
-        name: name,
-        email: email,
-        age: age,
-        adress: adress
-
-    });
-
-    localStorage.setItem("peopleList", JSON.stringify (peopleList));
-    showData();
-    document.getElementById("name").value="";
-    document.getElementById("email").value="";
-    document.getElementById("age").value="";
-    document.getElementById("adress").value="";
-    }
-
-    showData ();
 }
 
-function deleteData(index){
-    var peopleList;
-    if (localStorage.getItem('peopleList')==null){
-        peopleList = [];
-    } else {
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
-    }
-
+function deleteData(index) {
+    var peopleList = JSON.parse(localStorage.getItem('peopleList')) || [];
     peopleList.splice(index, 1);
     localStorage.setItem("peopleList", JSON.stringify(peopleList));
     showData();
 }
 
-function updateData(index){
+function updateData(index) {
     document.getElementById("submit").style.display = "none";
     document.getElementById("update").style.display = "block";
 
-    var peopleList;
-    if (localStorage.getItem('peopleList')==null){
-        peopleList = [];
-    } else {
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
-    }
+    var peopleList = JSON.parse(localStorage.getItem('peopleList')) || [];
+    document.getElementById("name").value = peopleList[index].name;
+    document.getElementById("email").value = peopleList[index].email;
+    document.getElementById("age").value = peopleList[index].age;
+    document.getElementById("adress").value = peopleList[index].adress;
 
-    document.getElementById("name").value = peopleList [index].name;
-    document.getElementById("email").value = peopleList [index].email;
-    document.getElementById("age").value = peopleList [index].age;
-    document.getElementById("adress").value = peopleList [index].adress;
-
-    document.querySelector("#updateData").addEventListener("click", function (){
-        if (validateForm() == true){
+    document.querySelector("#update").addEventListener("click", function () {
+        if (validateForm()) {
             peopleList[index].name = document.getElementById("name").value;
             peopleList[index].email = document.getElementById("email").value;
             peopleList[index].age = document.getElementById("age").value;
             peopleList[index].adress = document.getElementById("adress").value;
 
             localStorage.setItem("peopleList", JSON.stringify(peopleList));
-
             showData();
 
             document.getElementById('name').value = "";
@@ -141,7 +109,7 @@ function updateData(index){
             document.getElementById('adress').value = "";
 
             document.getElementById("submit").style.display = "block";
-    document.getElementById("update").style.display = "none";
+            document.getElementById("update").style.display = "none";
         }
-    })
+    });
 }
